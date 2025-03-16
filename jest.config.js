98db921c5eeedcf853f0054c0c52a0c38a96724e
Sebastian Gedda <sebastian.gedda@nltg.com>
2025-03-16 11:51:50 +0100

    fix: Exclude example projects from test runs

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/example-projects/', '/examples/'],
   moduleFileExtensions: ['ts', 'js'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
